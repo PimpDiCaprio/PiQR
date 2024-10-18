e103f3c5a04309393b0a4c02ffca2cdc0299da6f
PimpDiCaprio <62346071+PimpDiCaprio@users.noreply.github.com>
2024-10-18 13:27:32 -0500

    Update create_png.py

--- examples/create_png.py
+++ examples/create_png.py
@@ -1,1 +1,13 @@
+from pyqr import PyQR
 
+# define the input string for the qr code
+qr_string = 'Example Text'
+
+# Create a QReader instance
+pqr = PyQR.QR()
+
+# generate qr binary for the input string
+qr_output = pqr.generate_qr(qr_string, correction_level='Medium')
+
+# save qr binary as png
+PyQR.show_png(qr_output, 'test.png')
