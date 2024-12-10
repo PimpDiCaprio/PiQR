5f42d03f9f84d055940bcbebb4ca9a431eb3b0c4
PimpDiCaprio <gogoluba@gmail.com>
2024-12-10 11:18:28 -0600

    Updated the version to 0.1.2 and removed a print line from "qr_imager" that was unneccessary

--- setup.py
+++ setup.py
@@ -6,18 +6,18 @@ setup(
     author='PimpDiCaprio',
     author_email='info@aperturesoftware.us',
     description='A Python library for displaying QR codes.',
-    long_description=open('README.md').read(),  # Make sure you have a README.md file
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/PimpDiCaprio/PiQR',  # Update with your repository URL
-    packages=find_packages(),  # Automatically find packages in the current directory
+    url='https://github.com/PimpDiCaprio/PiQR',
+    packages=find_packages(),
     scripts=['PiQR.py'],
     install_requires=[
-        'opencv-contrib-python',  # Dependency for image processing
+        'opencv-contrib-python',
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',  # Specify your license
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',  # Specify your Python version requirements
+    python_requires='>=3.6',
 )
